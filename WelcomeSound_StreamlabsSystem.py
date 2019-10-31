7ae807391d32086c2a830faec5c11aab349ca6a8
bruno <bfl2@cin.ufpe.br>
2019-10-31 15:35:08 -0300

    Initial Commit

---
+++ WelcomeSound_StreamlabsSystem.py
@@ -0,0 +1,92 @@
+import sys
+import json
+import os
+import ctypes
+import codecs
+
+### Default Variables
+
+ScriptName = "Welcome Sound"
+Website = "github.org/bfl2"
+Description = "Script do canal ursope para tocar sons especiais para subs ao entrarem no canal"
+Creator="ursope"
+Version="1.0.0"
+
+configFile = "config.json"
+volume = 0.1
+command = "!WelcomeSound"
+soundspath = ""
+soundsAvailable = []
+greetedUsers = []
+
+verbose = True
+
+### Functions
+
+
+### Obligatory function
+def Init():
+    global soundsAvailable, soundspath, volume, words, settings
+    path = os.path.dirname(__file__)
+    soundspath = path + "/soundfiles"
+
+    updateVolumeReading()
+
+    soundsAvailable = os.listdir(soundspath)
+
+    return
+
+def Execute(data):
+    global greetedUsers, verbose
+    userId = data.User
+    username = data.UserName
+    paramCount = data.GetParamCount()
+
+    if data.GetParam(0) != command:
+        return
+    if userId not in greetedUsers:
+        playRandomSound()
+        greetedUsers.append(userId)
+
+    if verbose:
+        log("userId = "+str(userId))
+        log("userName = "+str(username))
+        log("paramCount = "+str(paramCount))
+        log("Greeted Users count = " + str(len(greetedUsers)))
+
+    return
+
+def Tick():
+    updateVolumeReading()
+    return
+
+### Auxiliary functions
+def updateVolumeReading():
+    global volume
+    path = os.path.dirname(__file__)
+    try:
+        with codecs.open(os.path.join(path, configFile), encoding='utf-8-sig', mode='r') as file:
+		    settings = json.load(file, encoding='utf-8-sig')
+    except:
+		settings = {
+			"liveOnly": True,
+			"volume": 50.0,
+		}
+
+    volume = settings["volume"]/100.0
+
+
+def playSound(soundFile):
+    soundFullpath = soundspath + "/" + soundFile
+    Parent.PlaySound(soundFullpath, volume)
+
+    return
+
+def playRandomSound():
+
+    seed = Parent.GetRandom(0, len(soundsAvailable) - 1)
+    return playSound(soundsAvailable[seed])
+
+def log(message):
+    Parent.Log(command, message)
+    return
---
+++ config.js
@@ -0,0 +1,14 @@
+﻿var settings = {
+  "liveOnly": true,
+  "words": "hey, hi, hello",
+  "permission": "Everyone",
+  "volume": 7,
+  "useCooldown": true,
+  "useCooldownMessages": false,
+  "cooldown": 1,
+  "onCooldown": "$user, $command is still on cooldown for $cd minutes!",
+  "userCooldown": 300,
+  "onUserCooldown": "$user, $command is still on user cooldown for $cd minutes!",
+  "responseHello": "Hello, $user!",
+  "singleGreeting": true
+};
